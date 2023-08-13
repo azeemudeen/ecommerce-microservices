9f20ab5bf6a55be11e54edeb9b155b9f9d098b1d
azeemudeen <azeemdheen7@gmail.com>
2023-08-14 00:35:04 +0530

    inventory-service and multi-maven project setup

--- order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
@@ -16,8 +16,7 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrders(orderRequest);
+        orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
-
 }
--- order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -7,29 +7,30 @@ import com.ecommerce.orderservice.model.OrderLineItems;
 import com.ecommerce.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public void placeOrders(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
-                .map(this::mapToOrderLineItemsDto)
+                .map(this::mapToOrderLineItems)
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
-
         orderRepository.save(order);
     }
 
-    private OrderLineItems mapToOrderLineItemsDto(OrderLineItemsDto orderLineItemsDto) {
+    private OrderLineItems mapToOrderLineItems(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
